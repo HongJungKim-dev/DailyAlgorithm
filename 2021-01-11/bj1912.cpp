b33c653726313e60cb06b5ab261dff442a2078ae
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-01-11 23:55:21 +0900

    make function
    separate many codes from main, so make function for continuous sum

--- 2021-01-11/bj1912.cpp
+++ 2021-01-11/bj1912.cpp
@@ -4,14 +4,15 @@
 
 using namespace std;
 
+int continuous_sum(vector<int>subTotal, vector<int> input, int loop);
+
 int main() {
 	int loop = 0;
 	vector<int> input;
 	vector<int> subTotal;
 	int num = 0;
 	int i = 0;
-	int res = -1000;
-
+	
 	cin >> loop;
 	input.assign(loop, 0);
 	subTotal.assign(loop, 0);
@@ -20,11 +21,17 @@ int main() {
 		cin >> input[i];
 	}
 
+	cout << continuous_sum(subTotal, input, loop);
+}
+
+int continuous_sum(vector<int>subTotal, vector<int> input, int loop) {
+	int i = 0;
+	int res = -1000;
 	subTotal[0] = input[0];
 	for (i = 1; i < loop; i++) {
-		subTotal[i] = max(subTotal[i-1] + input[i], input[i]);
+		subTotal[i] = max(subTotal[i - 1] + input[i], input[i]);
 		res = max(res, subTotal[i]);
 	}
 	res = max(res, subTotal[0]);
-	cout << res;
+	return res;
 }
