697259ab4fd47f838d8457eafd79ce6078a11080
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-02-02 23:36:19 +0900

    Solve with quotient, remainder
    ##Background  
    하샤드 수(https://programmers.co.kr/learn/courses/30/lessons/12947)  
    * 양의 정수의 각 자릿수의 합으로 x가 나누어지는지 구합니다.
    
    ##Requirement  
    * x는 1이상, 10000 이하인 정수입니다.
    
    ##Guide  
    *  10으로 몫과 나머지를 구하고 while문에서 이를 반복하면서 나머지는 계속 누적합을 구합니다.
    * x를 누적합으로 나누어서 그 값에 따라 if else로 정답을 저장하여 리턴합니다.
    * 누적합이 0인경우 ArithmeticException:/ by zero에러를 피하기 위해서 예외처리합니다.

---
+++ 2021-02-02/haShardNumber.js
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = false;
+        int quotient = 0;
+        int remainder = 0;
+        int accumulator = 0;
+        
+     
+ 
+        remainder = x%10;
+        quotient = x/10;
+        
+        accumulator += remainder;
+        
+        while(quotient != 0){
+            remainder = quotient%10;
+            quotient = quotient/10;
+            
+            accumulator += remainder;
+        }   
+        if( accumulator == 0 ){
+            return false;
+        }
+        if(x%accumulator == 0){
+            answer = true;
+        }
+        return answer;
+    }
+}
