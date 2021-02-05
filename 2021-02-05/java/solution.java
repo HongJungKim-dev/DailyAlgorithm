90bcf7bddef643898ac50237d196fd10be125369
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-02-05 20:04:54 +0900

    Update solution.java

--- 2021-02-05/java/solution.java
+++ 2021-02-05/java/solution.java
@@ -1,1 +1,15 @@
-
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int commandsLen = commands.length;
+        int[] answer = new int[commandsLen];
+        int start = 0;
+        for(int i=0; i<commandsLen; i++){
+            start = commands[i][0]-1;
+            Arrays.sort(answer, start, commands[i][1]);
+            answer[i] = answer[start + commands[i][2]];
+        }
+  
+        return answer;
+    }
+}
